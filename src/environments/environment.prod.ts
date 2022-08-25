401238b0ff848b6d171605241fd0b09ecae8390d
Johnny Silva <johnnysilva.dev@gmail.com>
2022-08-24 21:31:11 -0300

    adicionando params no ambiente de producao

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  PARAMS: ``,
+  PARAMS: `?ts=1589651945&apikey=caae684840600b730c185b0835eefa26&hash=b10757f591ca6001131641ab844d5d87`,
   BASE_URL: 'https://gateway.marvel.com:443/v1/public',
 };
